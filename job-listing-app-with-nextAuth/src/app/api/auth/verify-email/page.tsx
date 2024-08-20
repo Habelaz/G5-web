00141a38cb87a1a93d9f9fb88d352500143da89f
Abel Zereabruk <abelazereabruk@gmail.com>
2024-08-20 17:02:58 +0300

    take email from signup page

--- job-listing-app-with-nextAuth/src/app/api/auth/verify-email/page.tsx
+++ job-listing-app-with-nextAuth/src/app/api/auth/verify-email/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useVerifyEmailMutation } from '@/app/features/api';
 
@@ -9,7 +9,8 @@ const VerifyEmailPage = () => {
   const [verifyEmail, { isLoading }] = useVerifyEmailMutation();
   const router = useRouter();
   const searchParams = useSearchParams();
-  const email = 'abela.zereabiruk@gmail.com'
+  
+  const email = searchParams.get('email');
 
   const handleOtpChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const value = event.target.value;
@@ -19,6 +20,7 @@ const VerifyEmailPage = () => {
       return newOtp.join('');
     });
   };
+
   const handleContinue = async () => {
     if (!email) {
       setError('Email is missing. Please try again.');
@@ -44,33 +46,33 @@ const VerifyEmailPage = () => {
 
   return (
     <div className='flex justify-center items-center mt-[120px]'>
-    <div className=' w-[400px]'>
-      <p className='text-3xl text-center text-[#25324B] mb-8 font-bold'>Verify Email</p>
-      <p className='font-light'>
-        We've sent a verification code to the email address you provided. To complete the verification process, please enter the code here.
-      </p>
-      <div className='text-center mt-8 mb-2'>
-        {Array.from({ length: 4 }).map((_, index) => (
-          <input 
-            key={index}
-            className="border-purple-500 text-center bg-gray-100 border m-1 w-10 h-10 rounded-md" 
-            type="text" 
-            maxLength={1} 
-            value={otp[index] || ''} 
-            onChange={e => handleOtpChange(e, index)} 
-          />
-        ))}
-      </div>
-      <p className='font-light text-center'>You can request to <span className='font-normal text-[#4640DE]'>Resend code</span> in</p>
-      <p className='text-center text-[#4640DE]'><span>0:30</span></p>
-      <div className="text-center mt-5">
-        <button className='bg-[#4640DE] text-white w-[300px] h-9 rounded-md' onClick={handleContinue} disabled={isLoading}>
-          {isLoading ? 'Verifying...' : 'Continue'}
-        </button>
-      </div>
+      <div className=' w-[400px]'>
+        <p className='text-3xl text-center text-[#25324B] mb-8 font-bold'>Verify Email</p>
+        <p className='font-light'>
+          We've sent a verification code to the email address you provided. To complete the verification process, please enter the code here.
+        </p>
+        <div className='text-center mt-8 mb-2'>
+          {Array.from({ length: 4 }).map((_, index) => (
+            <input 
+              key={index}
+              className="border-purple-500 text-center bg-gray-100 border m-1 w-10 h-10 rounded-md" 
+              type="text" 
+              maxLength={1} 
+              value={otp[index] || ''} 
+              onChange={e => handleOtpChange(e, index)} 
+            />
+          ))}
+        </div>
+        <p className='font-light text-center'>You can request to <span className='font-normal text-[#4640DE]'>Resend code</span> in</p>
+        <p className='text-center text-[#4640DE]'><span>0:30</span></p>
+        <div className="text-center mt-5">
+          <button className='bg-[#4640DE] text-white w-[300px] h-9 rounded-md' onClick={handleContinue} disabled={isLoading}>
+            {isLoading ? 'Verifying...' : 'Continue'}
+          </button>
+        </div>
 
-      {error && <p className="text-red-500">{error}</p>}
-    </div>
+        {error && <p className="text-red-500">{error}</p>}
+      </div>
     </div>
   );
 };
